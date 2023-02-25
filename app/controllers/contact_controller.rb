476b55a35c4a6deaca8a9c1ead846467a6fa1fec
rabendra dev <rabemdrasharma001@gmail.com>
2023-02-25 19:23:53 +0530

    convert jquery logic into ruby on rails code

--- app/controllers/contact_controller.rb
+++ app/controllers/contact_controller.rb
@@ -1,7 +1,13 @@
 class ContactController < ApplicationController
 
   def index
-    @contacts = Contact.all.map{|contact| { 'name': contact.name, 'number': contact.contact_number }}
+    # @contacts = Contact.all.map{|contact| { 'name': contact.name, 'number': contact.contact_number }}
+  end
+
+  def get_contact
+    number = params[:number]
+    contact_details = Contact.find_by(contact_number: number)
+    render json: contact_details
   end
 
 end
